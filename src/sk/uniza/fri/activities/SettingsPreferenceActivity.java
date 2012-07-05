d0a6048e5b309d96a971b7f248034be6188a7483
lOlIl <peter.margetiak@gmail.com>
2012-07-05 13:47:52 +0200

    * fixed default server URL

--- src/sk/uniza/fri/activities/SettingsPreferenceActivity.java
+++ src/sk/uniza/fri/activities/SettingsPreferenceActivity.java
@@ -10,7 +10,7 @@ public class SettingsPreferenceActivity extends PreferenceActivity {
 	public static final String PREFERENCE_UPDATE = "updates_interval";
 	public static final String PREFERENCE_SERVER_URL = "server_url";
 	
-	public static final String SERVER_URL = "http://95.105.179.251";
+	public static final String SERVER_URL = "http://localhost";
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
